240990b1c960d7737c5281726f0088f57a9f8b14
Aleksey Kurepin <lesha@kurepin.com>
2022-05-29 16:00:59 +0300

    added creating table hits

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'sequel'
 
 DB = Sequel.connect(adapter: 'postgres', database: 'analytics')
 
+DB.create_table :hits do
+  primary_key :id
+  Inet :ip, null: false
+  String :user_agent, null: false
+  Time :created_at, null: false
+end unless DB.table_exists?(:hits)
+
 class App < Rack::App
   desc 'healthcheck endpoint'
   get '/' do
@@ -15,7 +22,7 @@ class App < Rack::App
       user_agent: request.env['HTTP_USER_AGENT'],
       created_at: Time.now
     )
-    # redirect_to 'https://www.youtube.com/channel/UCo4vIc1zKDHSioObftBYixg'
+    redirect_to 'https://www.youtube.com/channel/UCo4vIc1zKDHSioObftBYixg'
   end
 end
 
